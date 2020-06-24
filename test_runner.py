74252e4b03a11ea371687acc7d53ecb03e20ed60
Anya Tafliovich <atafliovich@utsc.utoronto.ca>
2020-06-24 17:42:06 -0400

    test_runner now takes a path to config.py file, instead of requiring it to be in the same directory.

--- test_runner.py
+++ test_runner.py
@@ -1,36 +1,35 @@
-#!/usr/bin/env python3
+'''Heart of uam.'''
 
-# Andrew Petersen (andrew.petersen@utoronto.ca)
-# May 8, 2014
+# Andrew Petersen (andrew.petersen@utoronto.ca) 2014
+# Anya Tafliovich 2019
 
 # Warning: Heavy use of os means this is probably a linux/unix only solution.
+# Note: I considered making the preamble and postamble Python code
+# (lambdas) but decided that most operations would be file system
+# prep/cleanup best performed in the shell.
+
+# Note: Neither preamble nor postamble are protected by timeouts.
+
+# Note: stdout and stderr in the subprocess calls could be redirected
+# for debug support.
+
+import argparse
+import importlib
 import os
 import signal
 import sys
 import multiprocessing
 import subprocess
 
-import argparse
 
-import config
+def execute_tests(student, config):
+    '''Run tests for one student.'''
 
-
-# Warning: Some of the exceptions in this code are new to Python 3.3.
-def execute_tests(student):
     try:
         os.chdir(student)
     except OSError:
         print('{}: Directory not found.'.format(student), file=sys.stderr)
         return
-
-    # Note: I considered making the preamble and postamble Python code
-    # (lambdas) but decided that most operations would be file system
-    # prep/cleanup best performed in the shell.
-
-    # Note: Neither preamble nor postamble are protected by timeouts.
-
-    # Note: stdout and stderr in the subprocess calls could be redirected
-    # for debug support.
 
     if config.preamble_cmd:
         try:
@@ -71,24 +70,29 @@ def execute_tests(student):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    PARSER = argparse.ArgumentParser(
         description='Executes unit tests on student code in each directory.')
 
-    # For testing on a subset of student directories.
-    parser.add_argument(
-        'students_fname', nargs='?',
-        default=config.students_fname,
-        help='File containing a list of student directories to test.')
+    PARSER.add_argument('config', help='Full path of configuration file.')
 
-    args = parser.parse_args()
+    ARGS = PARSER.parse_args()
 
-    # Configuration and setup
-    cwd = os.getcwd()
-    student_dirs = [
-        os.sep.join([cwd, student])
-        for student in open(args.students_fname).read().strip().split(os.linesep)]
+    HEAD, TAIL = os.path.split(ARGS.config)
+    sys.path.append(HEAD)
+    CONFIG = importlib.import_module(TAIL.split('.')[0])
 
-    procs = multiprocessing.Pool(config.max_processes)
+    def execute(student):
+        '''Execute tests for student with ARGS.config as config file.'''
 
-    # Walk directories, executing testcode in each directory.
-    procs.map(execute_tests, student_dirs)
+        execute_tests(student, CONFIG)
+
+    with open(CONFIG.students_fname) as fname:
+
+        STUDENT_DIRS = [
+            os.sep.join([os.getcwd(), student])
+            for student in fname.read().strip().split(os.linesep)]
+
+        PROCS = multiprocessing.Pool(CONFIG.max_processes)
+
+        # Walk directories, executing testcode in each directory.
+        PROCS.map(execute, STUDENT_DIRS)
