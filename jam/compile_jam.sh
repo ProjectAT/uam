0e1a4818797b93983c9279b1c93a93cabbb59ddc
Anya Tafliovich <atafliovich@utsc.utoronto.ca>
2020-08-30 18:36:20 -0400

    Added command line argument to JAM: the name of the output JSON file.

--- jam/compile_jam.sh
+++ jam/compile_jam.sh
@@ -2,7 +2,7 @@
 # Compiles the JAM source code and creates jam.jar
 
 JAMDIR=$(pwd)
-COMPILE_CP="-cp .:$JAMDIR/src:$JAMDIR/lib/junit-4.12.jar:$JAMDIR/lib/hamcrest-core-1.3.jar:$JAMDIR/lib/gson-2.8.5.jar"
+COMPILE_CP="-cp .:$JAMDIR/src:$JAMDIR/lib/junit-4.12.jar:$JAMDIR/lib/hamcrest-core-1.3.jar:$JAMDIR/lib/gson-2.8.5.jar:$JAMDIR/lib/commons-cli-1.4.jar"
 
 echo $JAMDIR
 echo $COMPILE_CP
--- jam/src/org/junit/runner/JAMCore.java
+++ jam/src/org/junit/runner/JAMCore.java
@@ -5,6 +5,14 @@ import java.io.PrintStream;
 
 import java.util.Arrays;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import org.junit.internal.RealSystem;
 import org.junit.internal.JUnitSystem;
 
@@ -15,29 +23,49 @@ public class JAMCore extends JUnitCore {
     public final static String DEFAULT_JSON_FILE = "result.json";
 
     /**
-     * Set up a JUnitSystem and overlaying JAMCore with a JSonOutputListener
-     * to generate JSON output from the results of running the provided
-     * test suite(s). If more than 1 argument is provided, the last argument
-     * is expected to be a path to an XML file containing exception
-     * explanations.
-     * @param args A list of paths to test suite(s) and possibly an XML file
+     * @param args A list of paths to test suite(s) 
+     * and optional output file name and exception explainer.
      */
     public static void main(final String... args) {
         JAMCore core = new JAMCore();
         JUnitSystem system = new RealSystem();
         Result result;
 
-        // If more than one argument is provided, expect the last
-        // argument to be a path to an XML file containing exception
-        // explanations. Pass it in separately when adding the listener
-        // and do not pass it to runMain()
+	// Parse command line arguments
+	Options options = new Options();
+
+        Option output = new Option("o", "output", true, "output file");
+        output.setRequired(false);  // not mandatory
+        options.addOption(output);
+
+        Option exceptions = new Option("e", "exceptions", true, "exceptions explainer");
+        exceptions.setRequired(false);  // not mandatory
+        options.addOption(exceptions);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
         try {
-            if (args.length > 1) {
+            cmd = parser.parse(options, args);
+	} catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("JAMCore", options);
+            System.exit(1);
+        }
+	
+	String outputFilePath = cmd.getOptionValue("output", DEFAULT_JSON_FILE);
+	String exceptionExplainer = cmd.getOptionValue("exceptions", "");  // TODO: change with JsonOutputListener 
+	String [] testers = cmd.getArgs(); // the rest of command-line arguments
+
+	// Set up corresponding Listener.
+        try {
+            if (exceptionExplainer.equals("")) {
                 core.addListener(new JSonOutputListener(
-                        new PrintStream(DEFAULT_JSON_FILE), args[args.length - 1]));
+                        new PrintStream(outputFilePath)));
             } else {
                 core.addListener(new JSonOutputListener(
-                        new PrintStream(DEFAULT_JSON_FILE)));
+                        new PrintStream(outputFilePath), exceptionExplainer));
             }
         } catch (FileNotFoundException e) {
             System.err.println("Unable to open report file for writing. " +
@@ -45,12 +73,8 @@ public class JAMCore extends JUnitCore {
             System.exit(1);
         }
 
-        if (args.length > 1) {
-            result = core.runMain(system,
-                                  Arrays.copyOfRange(args, 0, args.length - 1));
-        } else {
-            result = core.runMain(system, args);
-        }
+	// run the testers
+	result = core.runMain(system, testers);
 
         if (result.wasSuccessful()) {
             System.exit(0);
